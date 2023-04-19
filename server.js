da60c79c5dfd9252b59dc4dbc83ee3445f30e990
unknown <developer.mdshahid@gmail.com>
2023-04-19 16:19:02 +0530

    initial

---
+++ server.js
@@ -0,0 +1,20 @@
+import {app} from "./app.js";
+
+
+app.get("/", (req, res) => {
+    res.send("<h1>Home Page</h1>")
+})
+app.get("/about", (req, res) => {
+    res.send("<h1>About Page</h1>")
+})
+
+// Handling non matching request from the client
+app.use((req, res, next) => {
+    res.status(404).send(
+        "<h1>404 Page Not Found</h1>")
+})
+  
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is Working on Port:${process.env.PORT}`)
+})
