e5da4e8d84ed5f2b6937507b4e536702a3af68d7
unknown <developer.mdshahid@gmail.com>
2023-04-20 11:14:40 +0530

    Register form Added

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require('./app.js');
+const path = require('path');
+var bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
     res.send("<h1>Home PageS</h1>")
@@ -8,7 +11,20 @@ app.get("/about", (req, res) => {
     res.send("<h1>About Page</h1>")
 })
 
-// Handling non matching request from the client
+app.get("/register", (req, res) => {
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname)+'/register.html')
+})
+
+app.post("/api/v1/register", (req, res) => {
+    res.json({
+        Name : req.body.name,
+        Email : req.body.email,
+        Password : req.body.password,
+    })
+    
+    // res.send(`<h1>Name: ${req.body.name}</h1>`)
+})
 app.use((req, res, next) => {
     res.status(404).send(
         "<h1>404 Page Not Found</h1>")
